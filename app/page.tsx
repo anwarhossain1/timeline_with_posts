b7650f118467465fe00274e58d23303b49af7c59
Anwar Hossain <irishkhan33@gmail.com>
2024-06-09 11:30:54 +0600

    timeline posts data structure wise array building mechanism added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
 import { getMetadata } from "@/lib/utils";
 import type { Metadata } from "next";
-import { getPostsData } from "./server-utils";
-
+import {
+  getPostCommentsData,
+  getPostsData,
+  getUsersData,
+} from "./server-utils";
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 export const metadata: Metadata = getMetadata("home");
 
 const Home = async () => {
   const posts = await getPostsData();
-  console.log(posts);
+  const users = await getUsersData();
+  const postComments = await getPostCommentsData();
+  const timeLineContents = posts.map((post) => {
+    return {
+      title: post.title,
+      body: post.body,
+      comments: postComments.filter(
+        (comment: any) => comment.postId === post.id
+      ),
+      user: users.find((user: any) => user.id === post.userId),
+    };
+  });
+  console.log(timeLineContents);
   return <>{/* <BlogContent posts={getPostsData()} /> */}</>;
 };
 
